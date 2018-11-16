9eb9663ba058496a7779e77675f1b0aa41c986d7
tipstrade <john.bayly@tipstrade.net>
2018-11-16 20:22:21 +0000

    Disable the CreateOganisation tests

--- Tests/CreateUserTests.cs
+++ Tests/CreateUserTests.cs
@@ -1,12 +1,12 @@
 ﻿using Newtonsoft.Json;
-using System.Linq;
 using TipsTrade.HMRC.Api.CreateTestUser;
 using TipsTrade.HMRC.Api.CreateTestUser.Model;
 using Xunit;
 
 namespace TipsTrade.HMRC.Tests {
   public  class CreateUserTests :TestBase {
-    [Fact]
+    // Disable this by default do as not to create tonnes of users
+    ///[Fact]
     public void CreateOganisation() {
       var request = CreateTestUserFactory<CreateOrganisationRequest>.CreateTestUserFull();
       Assert.NotEmpty(request.ServiceNames);
