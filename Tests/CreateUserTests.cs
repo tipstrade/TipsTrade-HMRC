e83fef0ba651159dd8667a340817b416d851c5a4
tipstrade <john.bayly@tipstrade.net>
2018-11-16 20:20:51 +0000

    Improve test to check that all the values are populated

--- Tests/CreateUserTests.cs
+++ Tests/CreateUserTests.cs
@@ -1,4 +1,6 @@
-﻿using TipsTrade.HMRC.Api.CreateTestUser;
+﻿using Newtonsoft.Json;
+using System.Linq;
+using TipsTrade.HMRC.Api.CreateTestUser;
 using TipsTrade.HMRC.Api.CreateTestUser.Model;
 using Xunit;
 
@@ -8,9 +10,38 @@ namespace TipsTrade.HMRC.Tests {
     public void CreateOganisation() {
       var request = CreateTestUserFactory<CreateOrganisationRequest>.CreateTestUserFull();
       Assert.NotEmpty(request.ServiceNames);
-      Assert.Equal(11, request.ServiceNames.Count); // This is the number of Service Names as of v.1.0
+
+      // Compare to the request from the docs
+      var fromDocs = JsonConvert.DeserializeObject<CreateOrganisationRequest>(@"{
+  ""serviceNames"": [
+    ""corporation-tax"",
+    ""paye-for-employers"",
+    ""submit-vat-returns"",
+    ""national-insurance"",
+    ""self-assessment"",
+    ""mtd-income-tax"",
+    ""mtd-vat"",
+    ""lisa"",
+    ""secure-electronic-transfer"",
+    ""relief-at-source"",
+    ""customs-services""
+  ]
+}");
+
+      request.ServiceNames.Sort();
+      fromDocs.ServiceNames.Sort();
+
+      Assert.Equal(fromDocs.ServiceNames.Count, request.ServiceNames.Count);
+      for (int i = 0; i < request.ServiceNames.Count; i++) {
+        Assert.Equal(fromDocs.ServiceNames[i], request.ServiceNames[i]);
+      }
 
       var organisation = Client.CreateTestUser.CreateOrganisation(request);
+
+      foreach (var prop  in organisation.GetType().GetProperties()) {
+        var value = prop.GetValue(organisation);
+        Assert.NotNull(value);
+      }
     }
   }
 }
