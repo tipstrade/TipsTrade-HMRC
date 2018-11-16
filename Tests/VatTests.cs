bb7c9042c42e35738395c8652a760928b3ff0f1c
tipstrade <john.bayly@tipstrade.net>
2018-11-16 21:34:31 +0000

    Add more tests

--- Tests/VatTests.cs
+++ Tests/VatTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using TipsTrade.HMRC.Api.Vat.Model;
 using Xunit;
 using Xunit.Abstractions;
@@ -10,17 +11,55 @@ namespace TipsTrade.HMRC.Tests {
 
     [Fact]
     public void TestObligations() {
+      var year = DateTime.Now.Year;
+      if (DateTime.Now.Month < 3) {
+        year--;
+      }
+
       var obligations = new ObligationsRequest() {
         Vrn = OrganisationUser.Vrn,
-        From = new DateTime(2018, 1, 1),
-        To = new DateTime(2018, 11, 1).AddDays(-1),
-        Status = ObligationStatus.Fulfilled
+        From = new DateTime(year, 1, 1),
       };
+      obligations.To = obligations.From.AddYears(1).AddDays(-1);
 
       var client = Client;
       client.AccessToken = AccessToken;
 
-      var resp = client.Vat.GetObligations(obligations);
+      Obligation[] resp;
+
+      // All
+      resp = client.Vat.GetObligations(obligations);
+      Assert.NotNull(resp);
+      Assert.NotEmpty(resp);
+      foreach (var item in resp) {
+        Assert.NotEqual(default(DateTime), item.Start);
+        Assert.NotEqual(default(DateTime), item.End);
+        Assert.NotEqual(default(DateTime), item.Due);
+        Assert.NotNull(item.PeriodKey);
+      }
+
+      Output.WriteLine("VAT Obligations");
+      Output.WriteLine(JsonConvert.SerializeObject(resp, Formatting.Indented));
+
+      // Fulfulled
+      obligations.Status = ObligationStatus.Fulfilled;
+      resp = client.Vat.GetObligations(obligations);
+      Assert.NotNull(resp);
+      Assert.NotEmpty(resp);
+      foreach (var item in resp) {
+        Assert.Equal(ObligationStatus.Fulfilled, item.Status);
+        Assert.NotNull(item.Received);
+      }
+
+      // Open
+      obligations.Status = ObligationStatus.Open;
+      resp = client.Vat.GetObligations(obligations);
+      Assert.NotNull(resp);
+      Assert.NotEmpty(resp);
+      foreach (var item in resp) {
+        Assert.Equal(ObligationStatus.Open, item.Status);
+        Assert.Null(item.Received);
+      }
     }
   }
 }
