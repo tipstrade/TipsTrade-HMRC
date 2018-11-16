ad71d0fb5f92fb53ee7e853d5e08e9365518084d
tipstrade <john.bayly@tipstrade.net>
2018-11-16 21:09:55 +0000

    Add VAT obligations response

---
+++ TipsTrade-HMRC/Api/Vat/Model/Obligation.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TipsTrade.HMRC.Json.Converters;
+
+namespace TipsTrade.HMRC.Api.Vat.Model {
+  /// <summary>Represents a response containing VAT obligations.</summary>
+  public class Obligation {
+    /// <summary>The due date for this obligation period.</summary>
+    public DateTime Due { get; set; }
+
+    /// <summary>The end date of this obligation period.</summary>
+    public DateTime End { get; set; }
+
+    /// <summary>The ID code for the period that this obligation belongs to. The format is a string of four alphanumeric characters. Occasionally the format includes the # symbol.</summary>
+    public string PeriodKey { get; set; }
+
+    /// <summary>The obligation received date.</summary>
+    public DateTime? Received { get; set; }
+
+    /// <summary>The start date of this obligation period.</summary>
+    public DateTime Start { get; set; }
+
+    /// <summary>Which obligation statuses to return.</summary>
+    [JsonConverter(typeof(CharEnumConverter))]
+    public ObligationStatus Status { get; set; }
+  }
+}
---
+++ TipsTrade-HMRC/Api/Vat/Model/ObligationsResponse.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace TipsTrade.HMRC.Api.Vat.Model {
+  internal class ObligationsResponse {
+    public List<Obligation> Obligations { get; set; }
+  }
+}
