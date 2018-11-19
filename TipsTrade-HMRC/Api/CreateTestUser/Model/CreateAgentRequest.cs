408d0e87a3d5549ed809a5695fb8c2dbed0d4a2c
tipstrade <john.bayly@tipstrade.net>
2018-11-19 09:49:45 +0000

    Add agent and individual requests

---
+++ TipsTrade-HMRC/Api/CreateTestUser/Model/CreateAgentRequest.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace TipsTrade.HMRC.Api.CreateTestUser.Model {
+  /// <summary>The parameters used to create an organisation test user.</summary>
+  public class CreateAgentRequest : ICreateTestUserRequest {
+    /// <summary>Generates an Account Number for Agent Services and enrols the user for Agent Services.</summary>
+    [ServiceName]
+    public const string AgentServices = "agent-services";
+
+    /// <summary>The list of services that the user should be enrolled for.</summary>
+    [JsonProperty("serviceNames")]
+    public List<string> ServiceNames { get; set; } = new List<string>();
+  }
+}
---
+++ TipsTrade-HMRC/Api/CreateTestUser/Model/CreateIndividualRequest.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace TipsTrade.HMRC.Api.CreateTestUser.Model {
+  /// <summary>The parameters used to create an individual test user.</summary>
+  public class CreateIndividualRequest : ICreateTestUserRequest {
+    /// <summary>Generates an EORI number and enrols the user for Customs Services.</summary>
+    [ServiceName]
+    public const string CustomsServices = "customs-services";
+
+    /// <summary>Generates a National Insurance number and a Making Tax Digital Income Tax ID and enrols the user for Making Tax Digital Income Tax.</summary>
+    [ServiceName]
+    public const string MtdIncomeTax = "mtd-income-tax";
+
+    /// <summary>Generates a National Insurance number and enrols the user for National Insurance.</summary>
+    [ServiceName]
+    public const string NationalInsurance = "national-insurance";
+
+    /// <summary>Generates a Self Assessment UTR and enrols the user for Self Assessment.</summary>
+    [ServiceName]
+    public const string SelfAssessment = "self-assessment";
+
+    /// <summary>The list of services that the user should be enrolled for.</summary>
+    [JsonProperty("serviceNames")]
+    public List<string> ServiceNames { get; set; } = new List<string>();
+  }
+}
