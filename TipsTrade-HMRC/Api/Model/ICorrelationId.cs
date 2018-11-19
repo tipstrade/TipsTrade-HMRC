8d48127ef995e7ced072954cd4bad6bee41c9b36
tipstrade <john.bayly@tipstrade.net>
2018-11-19 17:15:28 +0000

    Fix typo

--- TipsTrade-HMRC/Api/Model/ICorrelationId.cs
+++ TipsTrade-HMRC/Api/Model/ICorrelationId.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace TipsTrade.HMRC.Api.Model {
-  /// <summary>Represents a model that provides provides a Coreelation ID.</summary>
+  /// <summary>Represents a model that provides provides a correlation ID.</summary>
   public interface ICorrelationId {
     /// <summary>Unique id for operation tracking String, 36 characters.</summary>
     Guid CorrelationId { get; set; }
