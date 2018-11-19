50baf22fec6f06d0c0802c50396fdeb32bd73513
tipstrade <john.bayly@tipstrade.net>
2018-11-19 10:08:58 +0000

    Move inner address class into own model class

---
+++ TipsTrade-HMRC/Api/CreateTestUser/Model/Address.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace TipsTrade.HMRC.Api.CreateTestUser.Model {
+  /// <summary>Represents an address.</summary>
+  public class Address {
+    /// <summary>Organisation's first line of address.</summary>
+    [JsonProperty("line1")]
+    public string Line1 { get; set; }
+
+    /// <summary>Organisation's second line of address.</summary>
+    [JsonProperty("line2")]
+    public string Line2 { get; set; }
+
+    /// <summary>Organisation's postcode.</summary>
+    [JsonProperty("postcode")]
+    public string Postcode { get; set; }
+  }
+}
--- TipsTrade-HMRC/Api/CreateTestUser/Model/OrganisationResult.cs
+++ TipsTrade-HMRC/Api/CreateTestUser/Model/OrganisationResult.cs
@@ -57,20 +57,5 @@ namespace TipsTrade.HMRC.Api.CreateTestUser.Model {
       [JsonProperty("address")]
       public Address Address { get; set; }
     }
-
-    /// <summary>Represents an address.</summary>
-    public class Address {
-      /// <summary>Organisation's first line of address.</summary>
-      [JsonProperty("line1")]
-      public string Line1 { get; set; }
-
-      /// <summary>Organisation's second line of address.</summary>
-      [JsonProperty("line2")]
-      public string Line2 { get; set; }
-
-      /// <summary>Organisation's postcode.</summary>
-      [JsonProperty("postcode")]
-      public string Postcode { get; set; }
-    }
   }
 }
