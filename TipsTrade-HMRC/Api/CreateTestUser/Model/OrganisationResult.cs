f6c4b182f10148b2869219e498e3768fa2948d3c
tipstrade <john.bayly@tipstrade.net>
2018-11-19 10:08:22 +0000

    Move shared properties into abstract base class

--- TipsTrade-HMRC/Api/CreateTestUser/Model/OrganisationResult.cs
+++ TipsTrade-HMRC/Api/CreateTestUser/Model/OrganisationResult.cs
@@ -2,23 +2,7 @@
 
 namespace TipsTrade.HMRC.Api.CreateTestUser.Model {
   /// <summary>Represents a response containing a created organisation.</summary>
-  public class OrganisationResult {
-    /// <summary>Government Gateway ID.</summary>
-    [JsonProperty("userId")]
-    public string UserId { get; set; }
-
-    /// <summary>Government Gateway password.</summary>
-    [JsonProperty("password")]
-    public string Password { get; set; }
-
-    /// <summary>Government Gateway user's full name.</summary>
-    [JsonProperty("userFullName")]
-    public string FullName { get; set; }
-
-    /// <summary>Government Gateway user's email address.</summary>
-    [JsonProperty("emailAddress")]
-    public string Email { get; set; }
-
+  public class OrganisationResult : UserResultBase {
     /// <summary>Organisation deatils.</summary>
     [JsonProperty("organisationDetails")]
     public Organisation OrganisationDetails { get; set; }
---
+++ TipsTrade-HMRC/Api/CreateTestUser/Model/UserResultBase.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace TipsTrade.HMRC.Api.CreateTestUser.Model {
+  /// <summary>Represents the base object from which CreateUser responses must inherit.</summary>
+  public abstract class UserResultBase {
+    /// <summary>Government Gateway ID.</summary>
+    [JsonProperty("userId")]
+    public string UserId { get; set; }
+
+    /// <summary>Government Gateway password.</summary>
+    [JsonProperty("password")]
+    public string Password { get; set; }
+
+    /// <summary>Government Gateway user's full name.</summary>
+    [JsonProperty("userFullName")]
+    public string FullName { get; set; }
+
+    /// <summary>Government Gateway user's email address.</summary>
+    [JsonProperty("emailAddress")]
+    public string Email { get; set; }
+  }
+}
