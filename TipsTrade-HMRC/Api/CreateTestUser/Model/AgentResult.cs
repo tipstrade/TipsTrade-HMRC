251ed9c658101daf57458bc76c80d8a2865f395a
tipstrade <john.bayly@tipstrade.net>
2018-11-19 10:13:03 +0000

    Add agent and individual results.

---
+++ TipsTrade-HMRC/Api/CreateTestUser/Model/AgentResult.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace TipsTrade.HMRC.Api.CreateTestUser.Model {
+  /// <summary>Represents a response containing a created agent.</summary>
+  public class AgentResult : UserResultBase {
+    /// <summary>Account number for Agent Service.</summary>
+    [JsonProperty("agentServicesAccountNumber")]
+    public string agentServicesAccountNumber { get; set; }
+  }
+}
---
+++ TipsTrade-HMRC/Api/CreateTestUser/Model/IndividualResult.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace TipsTrade.HMRC.Api.CreateTestUser.Model {
+  /// <summary>Represents a response containing a created individual.</summary>
+  public class IndividualResult : UserResultBase {
+    /// <summary></summary>
+    [JsonProperty("individualDetails")]
+    public Individual IndividualDetails { get; set; }
+
+    /// <summary>Self Assessment UTR.</summary>
+    [JsonProperty("saUtr")]
+    public string SelfAssessmentUtr { get; set; }
+
+    /// <summary>National Insurance number</summary>
+    [JsonProperty("nino")]
+    public string NiNumber { get; set; }
+
+    /// <summary>Economic Operator Registration and Identification (EORI) number.</summary>
+    [JsonProperty("eoriNumber")]
+    public string EoriNumber { get; set; }
+
+    /// <summary>Making Tax Digital Income Tax ID</summary>
+    [JsonProperty("mtdItId")]
+    public string MtdIncomeTaxId { get; set; }
+
+    /// <summary>Represents an individual.</summary>
+    public class Individual {
+      /// <summary>Individual's first name.</summary>
+      [JsonProperty("dateOfBirth")]
+      public DateTime DateOfBirth { get; set; }
+
+      /// <summary>Individual's last name.</summary>
+      [JsonProperty("firstName")]
+      public string FirstName { get; set; }
+
+      /// <summary>Individual's date of birth.</summary>
+      [JsonProperty("lastName")]
+      public string LastName { get; set; }
+
+      /// <summary>Organisation address.</summary>
+      [JsonProperty("address")]
+      public Address Address { get; set; }
+    }
+  }
+}
