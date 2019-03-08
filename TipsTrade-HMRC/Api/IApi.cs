90f6bc416a2baae190e5b8c43baba642620ec76d
tipstrade <john.bayly@tipstrade.net>
2019-03-08 15:06:29 +0000

    Add sample javascript for scraping the error constants from the docs

--- TipsTrade-HMRC/Api/IApi.cs
+++ TipsTrade-HMRC/Api/IApi.cs
@@ -1,6 +1,41 @@
 ﻿namespace TipsTrade.HMRC.Api {
   /// <summary>The description of a HMRC API.</summary>
   public interface IApi {
+    /*
+    // This code is used to parse the list of errors from the developer console of a documentaion page
+    var errors = {};
+    $("[data-heading='Code']").each(function(index, item) {
+      item = $(item);
+      var code = item.find("p").text();
+      var description = item.parent().find("[data-heading='Error Scenario'] p").text();
+
+      var list = errors[code] || [];
+      errors[code] = list;
+
+      var found = false;
+      for (var i = 0; i < list.length; i++) {
+        if (list[i] == description) {
+          found = true;
+          break;
+        }
+      }
+
+      if (!found) {
+        list.push(description);
+      }
+
+    });
+
+    var csharp = "";
+    Object.keys(errors).sort().forEach(function(code) {
+      csharp += "///<summary>\n///" + errors[code].join("\n///") + "\n///</summary>\n";
+      csharp += "public const string ERROR_" + code + " = \"" + code + "\";\n\n";
+    });
+
+    errors;
+    copy(csharp);
+     */
+
     /// <summary>The description of the API.</summary>
     string Description { get; }
 
