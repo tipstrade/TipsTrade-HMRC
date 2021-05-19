0759d60a95fdeece8fc6bc1b3f01a051ed3151fd
tipstrade <tipstrade@users.noreply.github.com>
2021-05-19 15:23:34 +0100

    Add extension method for generating header value from dictionary

--- TipsTrade-HMRC/Extensions/CollectionExtensions.cs
+++ TipsTrade-HMRC/Extensions/CollectionExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace System.Collections {
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace System.Collections {
   internal static class CollectionExtensions {
     public static bool Any(this IEnumerable value) {
       if (value == null) throw new ArgumentNullException();
@@ -7,6 +11,20 @@
         return true;
       }
       return false;
+    }
+
+    public static string GetHeaderValue<T>(this IDictionary<string, T> values) {
+      var sb = new StringBuilder();
+
+      foreach (var item in values) {
+        if (sb.Length != 0) {
+          sb.Append("&");
+        }
+
+        sb.AppendFormat("{0}={1}", HttpUtility.UrlEncode($"{item.Key}"), HttpUtility.UrlEncode($"{item.Value}"));
+      }
+
+      return sb.ToString();
     }
   }
 }
